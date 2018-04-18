8a52f11267cec36c4d157d996b94faa06f535938
Euan Macfarlane <euanmacfarlane@Euans-MacBook-Pro-2.local>
2018-04-18 12:32:59 +0100

    add modals to reducer

--- today-react-app/src/reducers/updateReducer.js
+++ today-react-app/src/reducers/updateReducer.js
@@ -4,76 +4,98 @@ const INITIAL_STATE = {
   isFetching: false,
   error: null,
   successMsg:null,
+  showDeleteModal: false,
+  updateToDelete: null,
+  showEditModal: false,
+  updateToEdit: null,
   newUpdate: null
 }
 
 export  const updateReducer = (currentState = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'FETCH_UPDATES_REQUEST':
-    return {
-      ...currentState,
-      updates:[],
-      update:null,
-      isFetching: true,
-      error: null,
-      successMsg:null,
-      updateToEdit: null,
-    }
+          return {
+            ...currentState,
+            updates:[],
+            update:null,
+            isFetching: true,
+            error: null,
+            successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'FETCH_UPDATES_SUCCESS':
-        return {
-          ...currentState,
-          updates:action.updates,
-          update:null,
-          isFetching: false,
-          error: null,
-          successMsg:action.message,
-          updateToEdit: null,
-        }
+          return {
+            ...currentState,
+            updates:action.updates,
+            update:null,
+            isFetching: false,
+            error: null,
+            successMsg:action.message,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'FETCH_UPDATES_FAILED':
-        return {
-          ...currentState,
-          updates:[],
-          update:null,
-          isFetching: false,
-          error: action.error,
-          successMsg:null,
-          updateToEdit: null,
-        }
+          return {
+            ...currentState,
+            updates:[],
+            update:null,
+            isFetching: false,
+            error: action.error,
+            successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'FETCH_UPDATE_REQUEST':
-        return {
-          ...currentState,
-          updates:currentState.updates,
-          update:null,
-          isFetching: true,
-          error: null,
-          successMsg:null,
-          updateToEdit: null,
-        }
+          return {
+            ...currentState,
+            updates:currentState.updates,
+            update:null,
+            isFetching: true,
+            error: null,
+            successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'FETCH_UPDATE_SUCCESS':
-        return {
-          ...currentState,
-          updates:currentState.updates,
-          update:action.update,
-          isFetching: false,
-          error: null,
-          successMsg:action.message,
-          updateToEdit: null,
-        }
+          return {
+            ...currentState,
+            updates:currentState.updates,
+            update:action.update,
+            isFetching: false,
+            error: null,
+            successMsg:action.message,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'FETCH_UPDATE_FAILED':
-        return {
-          ...currentState,
-          updates:[],
-          update:null,
-          isFetching: false,
-          error: action.error,
-          successMsg:null,
-          updateToEdit: null,
-        }
+          return {
+            ...currentState,
+            updates:[],
+            update:null,
+            isFetching: false,
+            error: action.error,
+            successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
+          }
 
     case 'ADD_NEW_UPDATE_REQUEST':
           return {
@@ -83,6 +105,10 @@ export  const updateReducer = (currentState = INITIAL_STATE, action) => {
             isFetching: true,
             error: null,
             successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
             newUpdate: action.update
           }
 
@@ -94,6 +120,10 @@ export  const updateReducer = (currentState = INITIAL_STATE, action) => {
             isFetching: true,
             error: action.error,
             successMsg:null,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
             newUpdate: null
           }
 
@@ -105,10 +135,44 @@ export  const updateReducer = (currentState = INITIAL_STATE, action) => {
             isFetching: false,
             error: null,
             successMsg:action.message,
+            showDeleteModal: false,
+            updateToDelete: null,
+            showEditModal: false,
+            updateToEdit: null,
             newUpdate: action.update
           }
         return nextState;
-      
+
+  case 'SHOW_EDIT_MODAL':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:null,
+          showDeleteModal: false,
+          updateToDelete: null,
+          showEditModal: true,
+          updateToEdit: action.update,
+          newUpdate: null
+        }
+
+  case 'HIDE_EDIT_MODAL':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:null,
+          showDeleteModal: false,
+          updateToDelete: null,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
     case 'EDIT_UPDATE_REQUEST':
           return {
             ...currentState,
@@ -125,12 +189,12 @@ export  const updateReducer = (currentState = INITIAL_STATE, action) => {
           }
 
     case 'EDIT_UPDATE_SUCCESS':
-        const updatedUpdates = currentState.updates.map((update) => {
-          if(update._id !== action.update._id){
-            return update;
-          }
-          return { ...update, ...action.update }
-        })
+         const updatedUpdates = currentState.updates.map((update) => {
+           if(update._id !== action.update._id){
+             return update;
+           }
+           return { ...update, ...action.update }
+         })
           return {
             ...currentState,
             updates:updatedUpdates,
@@ -145,20 +209,98 @@ export  const updateReducer = (currentState = INITIAL_STATE, action) => {
             newUpdate: null
           }
 
-    case 'EDIT_UPDATE_FAILED':
-          return {
-            ...currentState,
-            updates:currentState.updates,
-            update:null,
-            isFetching: false,
-            error: action.error,
-            successMsg:null,
-            showDeleteModal: false,
-            updateToDelete: null,
-            showEditModal: true,
-            updateToEdit: currentState.updateToEdit,
-            newUpdate: null
-          }
+  case 'EDIT_UPDATE_FAILED':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: action.error,
+          successMsg:null,
+          showDeleteModal: false,
+          updateToDelete: null,
+          showEditModal: true,
+          updateToEdit: currentState.updateToEdit,
+          newUpdate: null
+        }
+
+  case 'DELETE_UPDATE_REQUEST':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: true,
+          error: null,
+          successMsg:null,
+          showDeleteModal: true,
+          updateToDelete: action.update,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
+  case 'DELETE_UPDATE_SUCCESS':
+  const filteredUpdates = currentState.updates.filter((update) => update._id !== currentState.updateToDelete._id)
+        return {
+          ...currentState,
+          updates:filteredUpdates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:action.message,
+          showDeleteModal: true,
+          updateToDelete: null,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
+
+  case 'DELETE_UPDATE_FAILED':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: action.error,
+          successMsg:null,
+          showDeleteModal: true,
+          updateToDelete: null,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
+  case 'SHOW_DELETE_MODAL':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:null,
+          showDeleteModal: true,
+          updateToDelete: action.update,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
+  case 'HIDE_DELETE_MODAL':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:null,
+          showDeleteModal: false,
+          updateToDelete: null,
+          showEditModal: false,
+          updateToEdit: null,
+          newUpdate: null
+        }
+
 
     default:
        return currentState;
