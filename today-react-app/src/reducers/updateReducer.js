5c2f39cc183585aa1e8e22777b1e0d6a1fd4fddc
Euan Macfarlane <euanmacfarlane@Euans-MacBook-Pro-2.local>
2018-04-18 12:00:55 +0100

    add fetch updates to reducer

--- today-react-app/src/reducers/updateReducer.js
+++ today-react-app/src/reducers/updateReducer.js
@@ -4,12 +4,77 @@ const INITIAL_STATE = {
   isFetching: false,
   error: null,
   successMsg:null,
-  
   newUpdate: null
 }
 
 export  const updateReducer = (currentState = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'FETCH_UPDATES_REQUEST':
+    return {
+      ...currentState,
+      updates:[],
+      update:null,
+      isFetching: true,
+      error: null,
+      successMsg:null,
+      updateToEdit: null,
+    }
+
+    case 'FETCH_UPDATES_SUCCESS':
+        return {
+          ...currentState,
+          updates:action.updates,
+          update:null,
+          isFetching: false,
+          error: null,
+          successMsg:action.message,
+          updateToEdit: null,
+        }
+
+    case 'FETCH_UPDATES_FAILED':
+        return {
+          ...currentState,
+          updates:[],
+          update:null,
+          isFetching: false,
+          error: action.error,
+          successMsg:null,
+          updateToEdit: null,
+        }
+
+    case 'FETCH_UPDATE_REQUEST':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:null,
+          isFetching: true,
+          error: null,
+          successMsg:null,
+          updateToEdit: null,
+        }
+
+    case 'FETCH_UPDATE_SUCCESS':
+        return {
+          ...currentState,
+          updates:currentState.updates,
+          update:action.update,
+          isFetching: false,
+          error: null,
+          successMsg:action.message,
+          updateToEdit: null,
+        }
+
+    case 'FETCH_UPDATE_FAILED':
+        return {
+          ...currentState,
+          updates:[],
+          update:null,
+          isFetching: false,
+          error: action.error,
+          successMsg:null,
+          updateToEdit: null,
+        }
+
     case 'ADD_NEW_UPDATE_REQUEST':
           return {
             ...currentState,
